d09a0dea4bf70a18437903bbefe99a8a9472cae6
Pablo <p.navarros.2025@alumnos.urjc.es>
2026-02-22 14:08:54 +0100

    Final correction of count file exercise

--- S03/dna_count_file_corrected.py
+++ S03/dna_count_file_corrected.py
@@ -2,31 +2,13 @@ from S03.dna_count import count_bases
 file = open("dna.txt", "r")
 lines = file.readlines()
 file.close()
-print(count_bases(lines[1]))
 numberofsequences = len(lines)
-adenosine = 0
-guanine = 0
-thymine = 0
-cytosine = 0
-totalBases = 0
+totalCount = {"A": 0, "G": 0, "C": 0, "T": 0, "Total Bases": 0 }
 #use .strip() function to remove spaces and /n characters at the end of a string
 #do not copypaste the code, recall a function
 for j in range (0, numberofsequences):
     length = len(lines[j].strip())
     sequence = lines[j].strip()
-    for i in range (0, length):
-        if sequence[i] == 'A':
-            adenosine+= 1
-        elif sequence[i] == 'G':
-            guanine+= 1
-        elif sequence[i] == 'C':
-            cytosine+= 1
-        elif sequence[i] == 'T':
-            thymine+= 1
-        totalBases += 1
-print (lines)
-print("The total number of bases is " + str(totalBases))
-print("A: " + str(adenosine))
-print("G: " + str(guanine))
-print("C: " + str(cytosine))
-print("T: " + str(thymine))
+    totalCount = {k: totalCount[k] + count_bases(sequence)[k] for k in totalCount}
+
+print(totalCount)
