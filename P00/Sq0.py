7e3dce3a52db92783a004266898f978974c4bc00
Pablo <p.navarros.2025@alumnos.urjc.es>
2026-02-23 15:58:51 +0100

    Final correction of count file exercise

---
+++ P00/Sq0.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+def seq_ping():
+    print("ok")
+
+def seq_read_fasta(filename):
+    lines = Path(filename).read_text().splitlines()
+    del lines[0]
+    gene = "".join(lines)
+    return gene
+
+def seq_len(seq):
+    return len(seq)
+
+def seq_count_base(seq, base):
+    return seq.count(base)
+
+def seq_count(seq):
+    total_count = {"A": 0, "G": 0, "C": 0, "T": 0}
+    length = len(seq)
+    for j in range(0, length):
+        total_count[seq[j]] += 1
+    return total_count
+def seq_reverse(seq):
+    reversed_seq = ""
+    for i in range(0, len(seq)):
+        reversed_seq += (seq[len(seq) - 1 - i])
+
+    return reversed_seq
+def seq_complement(seq):
+    #With a translation method it is much easier
+    complementary_bases = str.maketrans("ATCG", "TAGC") #In the left the original characters and in the right the complements
+    return seq.translate(complementary_bases)
+
+print(seq_complement("ACGGT"))
