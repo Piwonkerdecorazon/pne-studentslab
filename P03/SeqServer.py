32d8e406a5dce1e9a518d94e2768082438a798e8
Piwonkerdecorazon <p.navarros.2025@alumnos.urjc.es>
2026-03-11 18:24:08 +0100

    Last modification

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -17,7 +17,7 @@ ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ls.bind((IP, PORT))
 # -- Step 3: Configure the socket for listening
 ls.listen()
-print("The server is configured!")
+print("Seq server is configured!")
 
 
 while True:
